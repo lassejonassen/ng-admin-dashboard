7151a3b0649da50d2184d0da6b1a6592a4c9530c
Lasse Jonassen <lasjn@arlafoods.com>
2023-05-28 23:28:31 +0200

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { ContentHeaderComponent } from './components/content-header/content-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreloaderComponent,
+    NavbarComponent,
+    MainSidebarComponent,
+    ContentHeaderComponent,
+    FooterComponent,
+    RightSidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
